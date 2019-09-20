976e19cea37c9e79251a4afdff26d98d6706c34a
UtsavisGreat <utsavbalar1231@gmail.com>
2019-09-20 14:17:01 +0000

    raphael: AOSIP bringup
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosip
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
@@ -85,8 +85,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:system/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl
 
 # Livedisplay
-PRODUCT_PACKAGES += \
-    lineage.livedisplay@2.0-service-sdm
+# PRODUCT_PACKAGES += \
+#     lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
